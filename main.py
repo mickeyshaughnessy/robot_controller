e7ab6bb5a738e4114e5ecc1bfa7f552260e3c9df
Mickey Shaughnessy <mickey.shaughnessy@vdx.tv>
2021-09-17 01:34:32 -0600

    update all

---
+++ main.py
@@ -0,0 +1,10 @@
+# This server is a central communication hub
+# for a system of household robots.
+
+# The server provides interfaces for controlling 
+# and monitoring the robots.
+
+# One control setup is through an http API - in principle, this provides an interface 
+# for external monitoring and control of the drone system.
+
+# A second interface is provided through a flight controller, connected to a standard xbox controller.
